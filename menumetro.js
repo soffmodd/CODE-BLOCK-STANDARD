ca203c3848a5c22e539ab3595772764755d2644a
soffmodd <soffmodd@yahoo.com>
2013-11-25 01:12:25 +0700

    Create menumetro.js

---
+++ menumetro.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+              $('.menumetro a').hover(function() {
+                $(this).stop().animate({opacity: 0.3}, 200);
+                 }, 
+                                 function() {
+               $(this).stop().animate({
+                opacity: 1
+                 }, 200);
+              });
+            });
