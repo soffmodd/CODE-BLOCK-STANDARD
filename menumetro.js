f3ba0b749728e04adfec974102a680d526c90e78
soffmodd <soffmodd@yahoo.com>
2013-11-25 07:58:02 +0700

    Update menumetro.js

--- menumetro.js
+++ menumetro.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
-              $('.menumetro a').hover(function() {
-                $(this).stop().animate({opacity: 0.3}, 200);
+              $('.menumetro a').hover(
+              function() {
+                            $(this).stop().animate({opacity: 0.3}, 200);
                  }, 
-                                 function() {
-               $(this).stop().animate({
-                opacity: 1
-                 }, 200);
+              function() {
+                            $(this).stop().animate({opacity: 1}, 200);
               });
             });
